98a4116830190c7d6366cf52abecb312a40fd145
Feeee <Fee@gmail.com>
2024-03-16 23:08:26 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿internal class Program
-{
+{   
+    public class HaloGeneric
+    {
+        public static void  SapaUser<T>(T data)
+        {
+            Console.WriteLine("Halo User " + data);
+        }
+    }
+
     private static void Main(string[] args)
     {
-        
+        HaloGeneric.SapaUser<String>("Febry");
     }
 }
