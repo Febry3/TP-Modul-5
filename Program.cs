817289c41a68945d821155cfee1606e9245e4229
Feeee <Fee@gmail.com>
2024-03-16 23:18:34 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 ﻿internal class Program
 {
+
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data) {  
+            this.data = data; 
+        }
+
+        public void PrintInfo() {
+            Console.WriteLine("Data yang tersimpan adalah: "+ this.data);
+        }
+    }
     private static void Main(string[] args)
     {
-        
+        DataGeneric<String> test = new DataGeneric<String>("1302220084");
+        test.PrintInfo();
     }
 }
